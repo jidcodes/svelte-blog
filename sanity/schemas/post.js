a9f3b34f1cac926697a9131bae574f06dbedc4a5
Jid <jidproxy@gmail.com>
2023-12-26 02:11:43 -1200

    v2

---
+++ sanity/schemas/post.js
@@ -0,0 +1,45 @@
+export default {
+  name: 'post',
+  type: 'document',
+  title: 'Post',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      options: {
+        source: 'title',
+      },
+    },
+    {
+      name: 'tag',
+      type: 'string',
+      title: 'Tag',
+    },
+    {
+      name: 'content',
+      type: 'array',
+      title: 'Content',
+      of: [
+        {
+          type: 'block',
+        },
+      ],
+    },
+    {
+      name: 'excerpt',
+      type: 'text',
+      title: 'Excerpt',
+    },
+    {
+      name: 'featuredImage',
+      type: 'image',
+      title: 'Featured Image',
+    },
+  ],
+}
